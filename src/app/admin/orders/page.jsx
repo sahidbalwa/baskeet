5e471d71190b249a098867b4f725112783cf9755
sahidbalwa <138686668+sahidbalwa@users.noreply.github.com>
2025-10-03 10:10:35 +0530

    second commit for fill the empty files.

---
+++ src/app/admin/orders/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Coming Soon...
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/PriceTag.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PriceTag = () => {
+  return (
+    <div>
+      Comming Soon...
+    </div>
+  )
+}
+
+export default PriceTag
